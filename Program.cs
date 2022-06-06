640f08b89512b711e90a9d0b53a74a0d86078ede
Guilherme Gonçalves Chagas <guilherme.chagas@programmers.com.br>
2022-06-06 10:39:52 -0300

    Inclusão do exercício

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int nota = 0;
+
+Console.WriteLine("Informe a nota do aluno: ");
+nota = int.Parse(Console.ReadLine());
+
+if (nota >= 0 && nota < 5)
+    Console.WriteLine("Reprovado");
+else if (nota >= 5 && nota < 7)
+    Console.WriteLine("Recuperação");
+else if (nota >= 7 && nota <= 10)
+    Console.WriteLine("Aprovado");
+else
+    Console.WriteLine("Nota inválida");
+
+var aprovado = (nota >= 7) ? "Aprovado" : "Recuperação";
+var situacao = nota >= 5 ? aprovado : "Reprovado";
+Console.WriteLine(situacao);
